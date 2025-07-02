844d230c60e9f56fc3a3ff1c1fcce46124986200
Vinícius Bornhofen <vinicius.bornhofen47@gmail.com>
2025-07-02 11:47:23 -0300

    estruturação

---
+++ src/entities/Produto.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity("produtos")
+export class Produto{
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    nome!: string;
+
+    @Column('decimal',{precision: 10, scale:2})
+    preco!: number;
+
+    @Column()
+    descricao!: string;
+}
