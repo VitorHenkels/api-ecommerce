2002839c16fddb3859e5198efb2a4ca395948708
Vinícius Bornhofen <vinicius.bornhofen47@gmail.com>
2025-07-14 09:01:48 -0300

    alterando autenticação do user

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -8,6 +8,6 @@ routes.get('/', authJwt, UsuarioController.getAll)
 routes.get('/:id', authJwt,UsuarioController.getOn)
 routes.put('/:id', authJwt,UsuarioController.update)
 routes.delete('/:id', authJwt,UsuarioController.delete)
-routes.post('/', authJwt,UsuarioController.create)
+routes.post('/',UsuarioController.create)
 
 export default routes;
